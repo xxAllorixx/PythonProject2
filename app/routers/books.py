460cc49f0e0dc7ec36da15408ab3592180bf9943
xxAllorixx <74657314+xxAllorixx@users.noreply.github.com>
2025-05-05 20:28:00 +0200

    Initial commit

--- app/routers/books.py
+++ app/routers/books.py
@@ -5,6 +5,7 @@ from app.data.books import book
 from typing import Annotated
 
 router = APIRouter(prefix="/books")
+
 @router.get("/")
 def get_all_books(sort: bool = False) -> list[Book]:
     """Returns the list of available books."""
@@ -12,6 +13,7 @@ def get_all_books(sort: bool = False) -> list[Book]:
         return sorted(book.values(), key=lambda book: book.review)
     else:
         return list(book.values())
+
 @router.post("/")
 def add_book(new_book: Book):
     """Adds a new book."""
@@ -19,11 +21,13 @@ def add_book(new_book: Book):
         raise HTTPException(status_code=403, detail="Book ID already exists.")
     book[new_book.id] = new_book
     return "Book successfully added."
+
 @router.delete("/")
 def delete_all_books():
     """Deletes all books."""
     book.clear()
     return "All books successfully deleted"
+
 @router.delete("/{id}")
 def delete_book(id: Annotated[int, Path(description="The ID of the book to delete")]):
     """Deletes the book with the given ID."""
@@ -32,6 +36,7 @@ def delete_book(id: Annotated[int, Path(description="The ID of the book to delet
         return "Book successfully deleted"
     except KeyError:
         raise HTTPException(status_code=404, detail="Book not found")
+
 @router.get("/{id}")
 def get_book_by_id(id: Annotated[int, Path(description="The ID of the book to get")]) -> Book:
     """Returns the book with the given id."""
@@ -39,6 +44,7 @@ def get_book_by_id(id: Annotated[int, Path(description="The ID of the book to ge
         return book[id]
     except KeyError:
         raise HTTPException(status_code=404, detail="Book not found")
+
 @router.post("/{id}/review")
 def add_review(id: Annotated[int, Path(description="The ID of the book to which add the review")],review: Review):
     """Adds a review to the book with the given ID."""
@@ -47,6 +53,7 @@ def add_review(id: Annotated[int, Path(description="The ID of the book to which 
         return "Review successfully added"
     except KeyError:
         raise HTTPException(status_code=404, detail="Book not found")
+
 @router.put("/{id}")
 def update_book(id: Annotated[int, Path(description="The ID of the book to update")], book: Book
 ):
